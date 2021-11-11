38d6c905715d36d740a75c507d74ca3d64503d1c
lucasfmoreno <mastropierodev@gmail.com>
2021-11-11 20:20:40 -0300

    Se agrego registro con nroRef automatico (random) y logueo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
 	protected
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.permit(:sign_up, keys:[:nombre,:dni,:edad,:apellido,:direccion,:rol,:diabetico,:enfermedadCardio,:enfermadadCardioDesc,:Otros])
+		devise_parameter_sanitizer.permit(:sign_in, keys: [:dni,:nroref])
 		devise_parameter_sanitizer.permit(:account_update, keys: [:direccion])
 	end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,23 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable 
-  before_save :default_rol
+         :recoverable, :rememberable, :validatable, :confirmable, :authentication_keys => [:nroref, :dni]
+        
+  before_save :default_rol, :asignar_nroTram
+
   def default_rol
     self.rol ||= "Paciente"
   end
+
+  def asignar_nroTram
+    nroRandom = rand(1000..9999)
+    puts "Genere nro random #{nroRandom}"
+    if(self.nroref == nil)
+      puts "Seteo nro random #{nroRandom}"
+      self.nroref = nroRandom
+    end
+  end
+
   validates :dni, uniqueness: true,presence: true
   validates :direccion, presence: true
   validates :edad, presence:true
