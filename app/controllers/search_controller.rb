c1f29aaf31e38eac5876721c1b6e62146b0951c7
MerOmega <insaurralde30@gmail.com>
2021-11-13 21:22:29 -0300

    Terminada Busqueda

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,7 @@ class SearchController < ApplicationController
 
     def search_user 
             @get_user = User.where(:dni => params[:dni]).first
+            @turno=Turno.where(:estado => "Aceptado").where(:user_id =>@get_user)
             @exists=true
     end
 end
