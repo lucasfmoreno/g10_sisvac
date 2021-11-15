24a2eb490761c646ddf8156568320bc801011e52
lucasfmoreno <mastropierodev@gmail.com>
2021-11-15 18:59:03 -0300

    Arreglado checkbox, funcionaba despues de actualizar.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    u = User.new(:id => 3, :edad =>4, :dni => 2999, :vacunatorio => "Terminal", :email => "unasdna@gmasdp.com", :direccion => "unadir", :nroref => 2143)
+    u.save
   end
 end
