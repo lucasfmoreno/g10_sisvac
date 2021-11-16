09992c0195b229fe86ec037fd0a1cd7b961d6ca3
lucasfmoreno <mastropierodev@gmail.com>
2021-11-16 17:53:34 -0300

    Se vacuna con FIEBRE AMARILLA a paciente creado por fuera de devise

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,7 +12,23 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
+    @usuarioLlego = params.require(:user).permit(:nombre,:email,:password,:dni,:edad,:apellido,:direccion,:vacunatorio,:rol,:diabetico,:enfermedadCardio,:enfermadadCardioDesc,:Otros)
+    puts "ESTOY EN CREATE DEVISE CON #{@usuarioLlego}"
+    @idNuevo = User.maximum(:id).next
     super
+    @usuarioCreado = User.find(@idNuevo)
+    puts "VOY A REVISAR #{@usuarioCreado} y #{@usuarioLlego}"
+    if(@usuarioCreado != nil)
+      puts "EN EL CREADO EL OTROS ES #{@usuarioCreado.Otros} y el DNI es #{@usuarioCreado.dni} == #{@usuarioLlego.require(:dni)}"
+      if(@usuarioCreado.email == @usuarioLlego.require(:email) && @usuarioCreado.Otros == "VACUNADOSINTURNO")
+       @turnoNuevo = Turno.new(:user_id => @usuarioCreado.id, :tipovacuna => "FIEBRE AMARILLA", :remember_created_at=>Date.today, :estado=>"Vacunado", :fechaRecibir =>Date.today,
+         :lugar => @user.vacunatorio)
+       @turnoNuevo.save
+       @vacunaNueva = VacunaDada.new(:user_id => @usuarioCreado.id, :turno_id => @turnoNuevo.id, :tipo_vacuna=>@turnoNuevo.tipovacuna, :fecha_solicitud=>@turnoNuevo.remember_created_at,
+          :fecha_dada=>@turnoNuevo.remember_created_at)
+       @vacunaNueva.save
+      end
+    end
   end
   private
 
