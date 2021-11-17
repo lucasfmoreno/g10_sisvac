109e58588cea193741a628394339c531b86da5ea
lucasfmoreno <mastropierodev@gmail.com>
2021-11-16 23:13:20 -0300

    Arreglado controller devise

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,30 +12,35 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
-    super
-    @usuarioLlego = params.require(:user).permit(:nombre,:email,:password,:dni,:edad,:apellido,:direccion,:vacunatorio,:rol,:diabetico,:enfermedadCardio,:enfermadadCardioDesc,:Otros)
+    if(params[:vengo_de_new]=="si")
+      puts "ENTRE POR EL NEW"
+      @usuarioLlego = params.require(:user).permit(:nombre,:email,:password,:dni,:edad,:apellido,:direccion,:vacunatorio,:rol,:diabetico,:enfermedadCardio,:enfermadadCardioDesc,:Otros)
 
-    @dniDelUltimo = User.find(User.maximum(:id)).id
-    
-    @usuarioCreado = User.find(User.maximum(:id))
+      @dniDelUltimo = User.find(User.maximum(:id)).dni
+      super
+      @usuarioCreado = User.find(User.maximum(:id))
 
-    @campoVacunadoSin = params[:vacunar_sin_turno]
-    @campoObsVac = params[:observacion_vacunacion]
+      @campoVacunadoSin = params[:vacunar_sin_turno]
+      @campoObsVac = params[:observacion_vacunacion]
 
-    #Antes de crear recupere el dni del Paciente con id maximo (el mas nuevo, a pesar de que se borren).
-    #Comparo ese dni con el de el ultimo agregado posterior al "super" que agrega en devise
-    #Si el dni es distinto es porque el Paciente con id maximo cambio, por lo tanto se agrego.
-    if(@usuarioCreado.id != @dniDelUltimo)
-      puts "EL CAMPO ES #{@campoVacunadoSin}, LA OBS ES #{@campoObsVac} y el DNI es #{@usuarioCreado.dni} == #{@usuarioLlego.require(:dni)}"
-      if(@usuarioCreado.email == @usuarioLlego.require(:email) && @campoVacunadoSin == "VACUNADOSINTURNO")
-       @turnoNuevo = Turno.new(:user_id => @usuarioCreado.id, :tipovacuna => "FIEBRE AMARILLA", :remember_created_at=>Date.today, :estado=>"Vacunado", :fechaRecibir =>Date.today,
-         :lugar => @user.vacunatorio, :observacion => @campoObsVac)
-       @turnoNuevo.save
-       @vacunaNueva = VacunaDada.new(:user_id => @usuarioCreado.id, :turno_id => @turnoNuevo.id, :tipo_vacuna=>@turnoNuevo.tipovacuna, :fecha_solicitud=>@turnoNuevo.remember_created_at,
+      #Antes de crear recupere el dni del Paciente con id maximo (el mas nuevo, a pesar de que se borren).
+      #Comparo ese dni con el de el ultimo agregado posterior al "super" que agrega en devise
+      #Si el dni es distinto es porque el Paciente con id maximo cambio, por lo tanto se agrego.
+      puts "EL DNI DEL CREADO ES #{@usuarioCreado.dni} == #{@dniDelUltimo}"
+      if(@usuarioCreado.dni != @dniDelUltimo)
+        puts "EL CAMPO ES #{@campoVacunadoSin}, LA OBS ES #{@campoObsVac} y el DNI es #{@usuarioCreado.dni} == #{@usuarioLlego.require(:dni)}"
+        if(@usuarioCreado.email == @usuarioLlego.require(:email) && @campoVacunadoSin == "VACUNADOSINTURNO")
+         @turnoNuevo = Turno.new(:user_id => @usuarioCreado.id, :tipovacuna => "FIEBRE AMARILLA", :remember_created_at=>Date.today, :estado=>"Vacunado", :fechaRecibir =>Date.today,
+          :lugar => @user.vacunatorio, :observacion => @campoObsVac)
+        @turnoNuevo.save
+        @vacunaNueva = VacunaDada.new(:user_id => @usuarioCreado.id, :turno_id => @turnoNuevo.id, :tipo_vacuna=>@turnoNuevo.tipovacuna, :fecha_solicitud=>@turnoNuevo.remember_created_at,
           :fecha_dada=>@turnoNuevo.remember_created_at, :observacion => @campoObsVac)
-       @vacunaNueva.save
+        @vacunaNueva.save
+        end
       end
-    end
+    else
+      super
+    end    
   end
   private
 
