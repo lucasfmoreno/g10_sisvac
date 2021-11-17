0fb0e53c21228eefb85b616ae39fa0df65e0c8ae
MerOmega <insaurralde30@gmail.com>
2021-11-17 00:00:48 -0300

    fecha hoy buscador

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
 
     def search_user 
             @get_user = User.where(:dni => params[:dni]).first
-            @turno=Turno.where(:estado => "Aceptado").where(:user_id =>@get_user)
+            @turno=Turno.where(:estado => "Aceptado").where(:user_id =>@get_user).where(:fechaRecibir=>Date.today)
             @exists=true
     end
 end
