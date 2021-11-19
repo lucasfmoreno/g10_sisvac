0568e2e11b4ae58879e2b7de9081318e344534a8
lucasfmoreno <mastropierodev@gmail.com>
2021-11-18 22:26:25 -0300

    Fibre amarilla para edad>60 y gripe si ya tiene un turno vacunado en menos de un año

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -13,29 +13,34 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # POST /resource
   def create
     if(params[:vengo_de_new]=="si")
-      puts "ENTRE POR EL NEW"
       @usuarioLlego = params.require(:user).permit(:nombre,:email,:password,:dni,:edad,:apellido,:direccion,:vacunatorio,:rol,:diabetico,:enfermedadCardio,:enfermadadCardioDesc,:Otros)
+      edad = Integer(@usuarioLlego.require(:edad))
+      if(edad>60)
+        flash[:error] = "El paciente no puede tener mas de 60 años."
+        @user=User.new(params.require(:user).permit(:nombre,:email,:password,:dni,:apellido,:direccion,:vacunatorio,:rol,:diabetico,:enfermedadCardio,:enfermadadCardioDesc,:Otros))
+        render "invitados/new"
+      else
+        @dniDelUltimo = User.find(User.maximum(:id)).dni
+        super
+        @usuarioCreado = User.find(User.maximum(:id))
 
-      @dniDelUltimo = User.find(User.maximum(:id)).dni
-      super
-      @usuarioCreado = User.find(User.maximum(:id))
+        @campoVacunadoSin = params[:vacunar_sin_turno]
+        @campoObsVac = params[:observacion_vacunacion]
 
-      @campoVacunadoSin = params[:vacunar_sin_turno]
-      @campoObsVac = params[:observacion_vacunacion]
-
-      #Antes de crear recupere el dni del Paciente con id maximo (el mas nuevo, a pesar de que se borren).
-      #Comparo ese dni con el de el ultimo agregado posterior al "super" que agrega en devise
-      #Si el dni es distinto es porque el Paciente con id maximo cambio, por lo tanto se agrego.
-      puts "EL DNI DEL CREADO ES #{@usuarioCreado.dni} == #{@dniDelUltimo}"
-      if(@usuarioCreado.dni != @dniDelUltimo)
-        puts "EL CAMPO ES #{@campoVacunadoSin}, LA OBS ES #{@campoObsVac} y el DNI es #{@usuarioCreado.dni} == #{@usuarioLlego.require(:dni)}"
-        if(@usuarioCreado.email == @usuarioLlego.require(:email) && @campoVacunadoSin == "VACUNADOSINTURNO")
-         @turnoNuevo = Turno.new(:user_id => @usuarioCreado.id, :tipovacuna => "FIEBRE AMARILLA", :remember_created_at=>Date.today, :estado=>"Vacunado", :fechaRecibir =>Date.today,
-          :lugar => @user.vacunatorio, :observacion => @campoObsVac)
-        @turnoNuevo.save
-        @vacunaNueva = VacunaDada.new(:user_id => @usuarioCreado.id, :turno_id => @turnoNuevo.id, :tipo_vacuna=>@turnoNuevo.tipovacuna, :fecha_solicitud=>@turnoNuevo.remember_created_at,
-          :fecha_dada=>@turnoNuevo.remember_created_at, :observacion => @campoObsVac)
-        @vacunaNueva.save
+        #Antes de crear recupere el dni del Paciente con id maximo (el mas nuevo, a pesar de que se borren).
+        #Comparo ese dni con el de el ultimo agregado posterior al "super" que agrega en devise
+        #Si el dni es distinto es porque el Paciente con id maximo cambio, por lo tanto se agrego.
+        puts "EL DNI DEL CREADO ES #{@usuarioCreado.dni} == #{@dniDelUltimo}"
+        if(@usuarioCreado.dni != @dniDelUltimo)
+          puts "EL CAMPO ES #{@campoVacunadoSin}, LA OBS ES #{@campoObsVac} y el DNI es #{@usuarioCreado.dni} == #{@usuarioLlego.require(:dni)}"
+          if(@usuarioCreado.email == @usuarioLlego.require(:email) && @campoVacunadoSin == "VACUNADOSINTURNO")
+          @turnoNuevo = Turno.new(:user_id => @usuarioCreado.id, :tipovacuna => "FIEBRE AMARILLA", :remember_created_at=>Date.today, :estado=>"Vacunado", :fechaRecibir =>Date.today,
+            :lugar => @user.vacunatorio, :observacion => @campoObsVac)
+          @turnoNuevo.save
+          @vacunaNueva = VacunaDada.new(:user_id => @usuarioCreado.id, :turno_id => @turnoNuevo.id, :tipo_vacuna=>@turnoNuevo.tipovacuna, :fecha_solicitud=>@turnoNuevo.remember_created_at,
+            :fecha_dada=>@turnoNuevo.remember_created_at, :observacion => @campoObsVac)
+          @vacunaNueva.save
+          end
         end
       end
     else
@@ -43,6 +48,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end    
   end
   private
+
+  
+  def unacosa
+    puts "JAJAJJAA"
+  end
 
   def sign_up_params
     params.require(:user).permit(:nombre,:email,:password,:dni,:edad,:apellido,:direccion,:vacunatorio,:rol,:enfermedadCardio)
