2a03b6168cc65a3240269bb361e3fff27d9c2691
lucasfmoreno <mastropierodev@gmail.com>
2021-11-30 01:59:45 -0300

    Buscar separado por admin y vacu. Informar dosis puestas de covid para vacunador al vacunar.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,13 @@
 class SearchController < ApplicationController
 
-    def search_user 
-            @get_user = User.where(:dni => params[:dni]).first
-            @turno=Turno.where(:estado => "Aceptado").where(:user_id =>@get_user).where(:fechaRecibir=>Date.today)
-            @exists=true
+    def search_user
+        puts "CURRENT ES #{current_user.rol}"
+        if(current_user.rol=="Administrador")
+            @get_user = User.where(:dni => params[:dni]).where(:rol => "Vacunador").first
+        elsif (current_user.rol=="Vacunador")
+            @get_user = User.where(:dni => params[:dni]).where(:rol => "Paciente").first
+        end
+        @turno=Turno.where(:estado => "Aceptado").where(:user_id =>@get_user).where(:fechaRecibir=>Date.today)
+        @exists=true
     end
 end
